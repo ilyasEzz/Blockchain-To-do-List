ed80abe0c632479336ff672d4fc0ef9f5b8d25f3
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-05-10 06:47:39 +0700

    Connecting Solidity with the Front-end

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,28 @@ pragma solidity ^0.5.0; // set the version
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id; // uint is an unsigned integer it can't be negative
+        string content;
+        bool completed;
+    }
+// mapping is like hash tables of key/value pairs
+    mapping(uint => Task ) public tasks;
+
+    function createTask(string memory _content) public {
+/*  mapping has a dynamic size in other words, 
+    solidity doesn't  know it size automaticly. This is why we need taskCount */
+     taskCount ++;
+
+// create a new Task(id, content, completed) and put it inside the mapping 
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    constructor() public {
+        createTask("Learning the Basics of Solidity");
+        createTask("Connecting Solidity with the Front-end ");
+    }
+
+
 }
